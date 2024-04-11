b31bb33ec46cfb4078bd7746f528634d39a51254
Hardeep Singh <hardeepsinghdal@gmail.com>
2024-04-12 00:55:41 +0530

    Commit it

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import socket
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
 
