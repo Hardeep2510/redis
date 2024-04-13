15f58b6ead2b2632aac5b4223e6e11982db1847e
Hardeep Singh <hardeepsinghdal@gmail.com>
2024-04-14 00:57:15 +0530

    commit it

--- app/main.py
+++ app/main.py
@@ -4,13 +4,15 @@ import socket
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-     print("Logs from your program will appear here!")
+    print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
 
-     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-     server_socket.accept() # wait for client
-
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket.accept() # wait for client
+    redis_socket, return_address = server_socket.accept()  # wait for client
+    print(return_address)
+    redis_socket.sendall(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
